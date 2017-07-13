b26ec8eacfc06af484779acdd830224f3b3f2400
Bhushan Khanale <bkhanale@gmail.com>
2017-07-13 09:23:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var articleOne = {
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
-    var content = data.heading;
+    var content = data.content;
     var date = data.date;
     var htmlTemplate = `
         <html>
