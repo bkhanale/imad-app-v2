0bc53e59879f51109771bd8ed22148a868321783
Bhushan Khanale <bkhanale@gmail.com>
2017-07-14 09:01:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,20 @@
 console.log('Loaded!');
 
+
+// Change the value of main-content
 var element = document.getElementById('main-content');
 element.innerHTML = 'New value';
+
+// Make the picture of madi move like in animation on clicking
+
+var img = document.getElementById('madi');
+var marginLeft = 0;
+
+function moveRight () {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+img.onClick = function() {
+    var interval = setInterval(moveRight,50);
+};
